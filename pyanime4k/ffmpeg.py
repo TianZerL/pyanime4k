d9f975c35dcb8ca0b3b17d77c5eeb218e5315399
k4yt3x <k4yt3x@k4yt3x.com>
2020-06-07 17:03:52 -0400

    re-written using python-ffmpeg and pathlib

--- pyanime4k/ffmpeg.py
+++ pyanime4k/ffmpeg.py
@@ -1,13 +1,26 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+"""
+Name: PyAnime4K FFmpeg handler
+Author: TianZerL
+Editor: K4YT3X
+"""
 
-import ffmpy3
+# built-in imports
+import pathlib
+
+# third-party imports
+import ffmpeg
 
 
-def mergeAudio(srcV, srcA, dst):
-    """Merge Audio from source file to the output file by FFmpeg"""
+def migrate_audio_streams(upscaled_video: pathlib.Path, original_video: pathlib.Path, output_path: pathlib.Path):
+    upscaled_input = ffmpeg.input(str(upscaled_video.absolute()))
+    original_input = ffmpeg.input(str(original_video.absolute()))
 
-    f = ffmpy3.FFmpeg(
-        inputs={srcV: None, srcA: None}, outputs={dst: "-c copy -map 0:0 -map 1:1 -y"}
-    )
-    f.run()
+    # find upscaled video stream and original audio stream
+    upscaled_video = upscaled_input.video
+    original_audio = original_input.audio
+
+    # create output file with selected streams
+    output = ffmpeg.output(upscaled_video, original_audio, str(output_path.absolute()))
+    output.run()
