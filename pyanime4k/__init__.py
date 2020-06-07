bbb44f6e1db0cc382e38840f8a0477e80d6cc2fd
k4yt3x <k4yt3x@k4yt3x.com>
2020-06-07 17:03:34 -0400

    added metadata

--- pyanime4k/__init__.py
+++ pyanime4k/__init__.py
@@ -1,5 +1,10 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+"""
+Name: PyAnime4K module initialization file
+Author: TianZerL
+Editor: K4YT3X
+"""
 
 from pyanime4k.anime4k.anime4kcpp import Anime4K
 from pyanime4k.upscale import *
