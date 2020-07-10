36fe818837e4c4c0a43be2c578201462511313d6
TianZerL <TianLin2112@outlook.com>
2020-07-10 19:25:18 +0800

    Update version

--- pyanime4k/ac.py
+++ pyanime4k/ac.py
@@ -22,7 +22,7 @@ class Version(object):
         self.core = str(ac_version.coreVersion, "utf-8")
         self.wrapper = str(ac_version.wrapperVersion, "utf-8")
 
-    pyanime4k = "2.2.6"
+    pyanime4k = "2.2.7"
 
 
 class Parameters(object):
